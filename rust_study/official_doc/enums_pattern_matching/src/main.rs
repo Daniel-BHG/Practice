80f4c4a29ab381d145f7f980e539617302ff16b5
Daniel <daniel@Daniel-MBP.local>
2025-08-26 08:22:18 +0900

    update

--- rust_study/official_doc/enums_pattern_matching/src/main.rs
+++ rust_study/official_doc/enums_pattern_matching/src/main.rs
@@ -75,6 +75,19 @@ fn main() {
 
         }
     }
+
+    // let .. else
+    fn describe_state_quarter(coin: Coin) -> Option<String> {
+        let Coin::Quarter(state) = coin else {
+            return None;
+        };
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old for America"));
+        }
+        else {
+            Some(format!("{state:?} is relatively new"));
+        }
+    }
 }
 
 enum IpAddrKind_only_name {
